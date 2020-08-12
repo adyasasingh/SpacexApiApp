c9b658702fbecd083cd9d5f76e1d5b5b87189468
ADYASA SINGH <adyasasingh5@gmail.com>
2020-08-13 00:34:20 +0530

    Spacex App

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public launch_date: any = [];
+  public launch_success: string ;
+  public land_success: string ;
+  public rocket:any= {};
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(){
+   
+    this.http.get('https://api.spaceXdata.com/v3/launches?limit=100')
+      .subscribe((launch_data) => {
+         // debugger;
+          this.launch_date = launch_data;
+          this.rocket =this.launch_date[1]['rocket'];
+          console.log(this.rocket)
+         // console.log(this.launch_date[1]['rocket']['first_stage']['cores'][0]['land_success']);
+      }, (err) => {
+          console.log(err);
+    });
+  }
+  onchangeDate($event){
+
+      this.http.get('https://api.spaceXdata.com/v3/launches?limit=100&launch_year='+$event)
+      .subscribe((launch_data) => {
+        
+          this.launch_date = launch_data;
+      }, (err) => {
+          console.log(err);
+    }); 
+  }
+
+  onLaunchChange($event){
+      this.http.get('https://api.spacexdata.com/v3/launches?limit=100&launch_success='+$event)
+      .subscribe((launch_data) => {
+      console.log($event);
+          this.launch_date = launch_data;
+      }, (err) => {
+          console.log(err);
+    }); 
+    
+  }
+  onLandingChange($event){
+      this.http.get('https://api.spacexdata.com/v3/launches?limit=100&land_success='+$event)
+      .subscribe((launch_data) => {
+        console.log($event);
+          this.launch_date = launch_data;
+      }, (err) => {
+          console.log(err);
+    }); 
+  }
+}
