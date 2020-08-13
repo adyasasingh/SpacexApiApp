3b210cf463a5726178aaed654d930df8737c453e
ADYASA SINGH <adyasasingh5@gmail.com>
2020-08-13 18:22:45 +0530

    new build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxSpinnerModule } from "ngx-spinner";  
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
